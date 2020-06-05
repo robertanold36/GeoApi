1b333314fb2c018248f08e5cd68c3fc3e9f2443e
robertanold <robertanold70@gmail.com>
2020-06-05 18:13:22 +0300

    port correction

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path=require('path');
 const app=express();
 require('dotenv').config();
 
-const port=8080;
+const port=process.env.PORT||8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended:false }));
